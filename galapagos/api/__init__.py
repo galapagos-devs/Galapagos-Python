3a139e41306e0c9953045efec149e8a1b8635695
ShaneKosieradzki <shanekos1@gmail.com>
2019-08-26 18:42:24 -0500

    Updated __galapagos__ to import .so for linux and .dll for windows

--- galapagos/api/__init__.py
+++ galapagos/api/__init__.py
@@ -1,2 +1,5 @@
-from ctypes import cdll
-__galapagos__ = cdll.LoadLibrary(r'C:\Users\asus\github\Galapagos\galapagos.so')
+from ctypes import cdll, windll
+import os
+
+loader = windll if os.system == 'win32' else cdll
+__galapagos__ = loader.LoadLibrary(r'C:\Users\asus\github\Galapagos\galapagos.so')
