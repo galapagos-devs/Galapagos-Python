99b911e326f5ccfc22ad495686157bff3ee152ee
kosieram <mitchkos21@gmail.com>
2019-10-22 18:21:34 -0500

    c-style metadata structs using ctypes library

--- galapagos/api/__init__.py
+++ galapagos/api/__init__.py
@@ -1,97 +1,77 @@
-from ctypes import *
+import ctypes
 import os
 
-loader = windll if os.system == 'win32' else cdll
+loader = ctypes.windll if os.system == 'win32' else ctypes.cdll
 __galapagos__ = loader.LoadLibrary(r'C:\Users\kosie\github\Galapagos\bin\Debug\SharedGalapagos.dll')
 
-#--------------------------
-#----Galapagos Session-----
-#--------------------------
-__galapagos__.create_population.restype = c_void_p
-__galapagos__.delete_population.argtypes = [c_void_p]
+fitness_func_t = ctypes.CFUNCTYPE(ctypes.c_double, ctypes.c_void_p)
 
-#----------------------------
-#----Chromosome Metadata-----
-#----------------------------
-__galapagos__.create_chromosome_metadata.restype = c_void_p
-__galapagos__.delete_chromosome_metadata.argtypes = [c_void_p]
 
-__galapagos__.set_chromosome_metadata_name.argtypes = [c_void_p, c_char_p]
-__galapagos__.get_chromosome_metadata_name.restype = c_char_p
-__galapagos__.get_chromosome_metadata_name.argtypes = [c_void_p]
+class CrossoverMetadata(ctypes.Structure):
+    _fields_ = []
 
-__galapagos__.set_chromosome_metadata_crossover_rate.argtypes = [c_void_p, c_double]
-__galapagos__.get_chromosome_metadata_crossover_rate.restype = c_double
-__galapagos__.get_chromosome_metadata_crossover_rate.argtypes = [c_void_p]
 
-__galapagos__.set_chromosome_metadata_mutation_rate.argtypes = [c_void_p, c_double]
-__galapagos__.get_chromosome_metadata_mutation_rate.restype = c_double
-__galapagos__.get_chromosome_metadata_mutation_rate.argtypes = [c_void_p]
+class MutationMetadata(ctypes.Structure):
+    _fields_ = []
 
-__galapagos__.set_chromosome_metadata_num_crossovers.argtypes = [c_void_p, c_size_t]
-__galapagos__.get_chromosome_metadata_num_crossovers.restype = c_size_t
-__galapagos__.get_chromosome_metadata_num_crossovers.argtypes = [c_void_p]
 
-__galapagos__.set_chromosome_metadata_crossovers.argtypes = [c_void_p, c_void_p]
-__galapagos__.get_chromosome_metadata_crossovers.restype = c_void_p
-__galapagos__.get_chromosome_metadata_crossovers.argtypes = [c_void_p]
+class SelectionAlgorithmMetadata(ctypes.Structure):
+    _fields_ = []
 
-__galapagos__.set_chromosome_metadata_num_mutations.argtypes = [c_void_p, c_size_t]
-__galapagos__.get_chromosome_metadata_num_mutations.restype = c_size_t
-__galapagos__.get_chromosome_metadata_num_mutations.argtypes = [c_void_p]
 
-__galapagos__.set_chromosome_metadata_mutations.argtypes = [c_void_p, c_void_p]
-__galapagos__.get_chromosome_metadata_mutations.restype = c_void_p
-__galapagos__.get_chromosome_metadata_mutations.argtypes = [c_void_p]
+class TerminationConditionMetadata(ctypes.Structure):
+    _fields_ = []
 
-#----------------------------
-#----Population Metadata-----
-#----------------------------
-fitness_func_t = CFUNCTYPE(c_double, c_void_p)
 
-__galapagos__.create_population_metadata.restype = c_void_p
-__galapagos__.delete_population_metadata.argtypes = [c_void_p]
+class ChromosomeMetadata(ctypes.Structure):
+    _fields_ = [('name', ctypes.c_char_p),
+                ('crossover_rate', ctypes.c_double),
+                ('mutation_rate', ctypes.c_double),
+                ('num_crossovers', ctypes.c_size_t),
+                ('crossover_metadata', ctypes.POINTER(ctypes.POINTER(CrossoverMetadata))),
+                ('num_mutations', ctypes.c_size_t),
+                ('mutation_metadata', ctypes.POINTER(ctypes.POINTER(MutationMetadata)))]
 
-__galapagos__.set_population_metadata_size.argtypes = [c_void_p, c_longlong]
-__galapagos__.get_population_metadata_size.restype = c_longlong
-__galapagos__.get_population_metadata_size.argtypes = [c_void_p]
+    def __init__(self, num_crossovers, num_mutations):
+        super().__init__()
+        crossover_elms = (ctypes.POINTER(CrossoverMetadata) * num_crossovers)()
+        mutation_elms = (ctypes.POINTER(MutationMetadata) * num_mutations)()
 
-__galapagos__.set_population_metadata_survival_rate.argtypes = [c_void_p, c_double]
-__galapagos__.get_population_metadata_survival_rate.restype = c_double
-__galapagos__.get_population_metadata_survival_rate.argtypes = [c_void_p]
+        self.num_crossovers = num_crossovers
+        self.crossover_metadata = ctypes.cast(crossover_elms,ctypes.POINTER(ctypes.POINTER(CrossoverMetadata)))
+        self.num_mutations = num_mutations
+        self.mutation_metadata = ctypes.cast(crossover_elms, ctypes.POINTER(ctypes.POINTER(MutationMetadata)))
 
-__galapagos__.set_population_metadata_distance_threshold.argtypes = [c_void_p, c_double]
-__galapagos__.get_population_metadata_distance_threshold.restype = c_double
-__galapagos__.get_population_metadata_distance_threshold.argtypes = [c_void_p]
 
-__galapagos__.set_population_metadata_cooperative_coevolution.argtypes = [c_void_p, c_bool]
-__galapagos__.get_population_metadata_cooperative_coevolution.restype = c_bool
-__galapagos__.get_population_metadata_cooperative_coevolution.argtypes = [c_void_p]
+class PopulationMetadata(ctypes.Structure):
+    _fields_ = [('size', ctypes.c_size_t),
+                ('survival_rate', ctypes.c_double),
+                ('distance_threshold', ctypes.c_double),
+                ('cooperative_coevolution', ctypes.c_bool),
+                ('num_selection_algorithms', ctypes.c_size_t),
+                ('selection_algorithm_metadata', ctypes.POINTER(ctypes.POINTER(SelectionAlgorithmMetadata))),
+                ('num_termination_conditions', ctypes.c_size_t),
+                ('termination_condition_metadata', ctypes.POINTER(ctypes.POINTER(TerminationConditionMetadata))),
+                ('fitness_function', fitness_func_t),
+                ('num_chromosomes', ctypes.c_size_t),
+                ('chromosome_metadata', ctypes.POINTER(ctypes.POINTER(ChromosomeMetadata)))]
 
-__galapagos__.set_population_metadata_num_selection_algorithm_metadata.argtypes = [c_void_p, c_longlong]
-__galapagos__.get_population_metadata_num_selection_algorithm_metadata.restype = c_longlong
-__galapagos__.get_population_metadata_num_selection_algorithm_metadata.argtypes = [c_void_p]
+    def __init__(self, num_selection_algorithms, num_termination_conditions, num_chromosomes):
+        super().__init__()
+        selection_algorithm_elms = (ctypes.POINTER(SelectionAlgorithmMetadata) * num_selection_algorithms)()
+        termination_condition_elms = (ctypes.POINTER(TerminationConditionMetadata) * num_termination_conditions)()
+        chromosome_elms = (ctypes.POINTER(ChromosomeMetadata) * num_chromosomes)()
 
-__galapagos__.set_population_metadata_selection_algorithm_metadata.argtypes = [c_void_p, c_void_p]
-__galapagos__.get_population_metadata_selection_algorithm_metadata.restype = c_void_p
-__galapagos__.get_population_metadata_selection_algorithm_metadata.argtypes = [c_void_p]
+        self.num_selection_algorithms = num_selection_algorithms
+        self.selection_algorithm_metadata = ctypes.cast(selection_algorithm_elms, ctypes.POINTER(ctypes.POINTER(SelectionAlgorithmMetadata)))
+        self.num_termination_conditions = num_termination_conditions
+        self.termination_condition_metadata = ctypes.cast(termination_condition_elms, ctypes.POINTER(ctypes.POINTER(TerminationConditionMetadata)))
+        self.num_chromosomes = num_chromosomes
+        self.chromosome_metadata = ctypes.cast(chromosome_elms, ctypes.POINTER(ctypes.POINTER(ChromosomeMetadata)))
 
-__galapagos__.set_population_metadata_num_termination_condition_metadata.argtypes = [c_void_p, c_longlong]
-__galapagos__.get_population_metadata_num_termination_condition_metadata.restype = c_longlong
-__galapagos__.get_population_metadata_num_termination_condition_metadata = [c_void_p]
 
-__galapagos__.set_population_metadata_termination_condition_metadata.argtypes = [c_void_p, c_void_p]
-__galapagos__.get_population_metadata_termination_condition_metadata.restype = c_void_p
-__galapagos__.get_population_metadata_termination_condition_metadata.argtypes = [c_void_p]
-
-__galapagos__.set_population_metadata_fitness_function.argtypes = [c_void_p, fitness_func_t]
-__galapagos__.get_population_metadata_fitness_function.restype = fitness_func_t
-__galapagos__.get_population_metadata_fitness_function.argtypes = [c_void_p]
-
-__galapagos__.set_population_metadata_num_chromosome_metadata.argtypes = [c_void_p, c_longlong]
-__galapagos__.get_population_metadata_num_chromosome_metadata.restype = c_longlong
-__galapagos__.get_population_metadata_num_chromosome_metadata.argtypes = [c_void_p]
-
-__galapagos__.set_population_metadata_chromosome_metadata.argtypes = [c_void_p, c_void_p]
-__galapagos__.get_population_metadata_chromosome_metadata.restype = c_void_p
-__galapagos__.get_population_metadata_chromosome_metadata.argtypes =[] c_void_p]
+# --------------------------
+# ----Galapagos Session-----
+# --------------------------
+__galapagos__.create_population.restype = ctypes.c_void_p
+__galapagos__.delete_population.argtypes = [ctypes.c_void_p]
